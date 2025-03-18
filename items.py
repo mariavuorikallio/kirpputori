07169424d857dc8d6fd4637264bd303d3d103982
Vuorikallio Maria A A <vuomaria@cubbli24-gold.cs.helsinki.fi>
2025-03-18 18:40:38 +0200

    Show item list/info

--- items.py
+++ items.py
@@ -4,3 +4,17 @@ def add_item(title, description, start_price, user_id):
     sql = """INSERT INTO items (title, description, start_price, user_id) VALUES (?, ?, ?, ?)"""
     db.execute(sql, [title, description, start_price, user_id])
 
+def get_items():
+    sql = "SELECT id, title FROM items ORDER BY id DESC"
+    return db.query(sql)
+
+def get_item(item_id):
+    sql = """SELECT items.title,
+                    items.description,
+                    items.start_price,
+                    users.username
+             FROM items
+             JOIN users ON items.user_id = users.id
+             WHERE items.id = ?"""
+    return db.query(sql, [item_id])[0]
+
